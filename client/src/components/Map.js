df557507281c3a4386598abef55632f8a4051030
Heather King <hkingdevelopment@gmail.com>
2021-12-04 18:54:05 +0000

    adds filter buttons - not yet linked

--- client/src/components/Map.js
+++ client/src/components/Map.js
@@ -20,54 +20,69 @@ const markers = [
 
 const Map = ( {setTooltipContent} ) => {
 
+
+    l
+
+
+
+
     return (
         <>
-        <h1>Map</h1>
-        <div>
-            <ComposableMap data-tip="" projectionConfig={{ scale: 200 }}>
-                <Geographies geography={geoUrl}>
-                    {({geographies}) => geographies.map(geo =>
-                            <Geography 
-                                key={geo.rsmKey} 
-                                geography={geo} 
-                                onMouseEnter={() => {
-                                    const { NAME } = geo.properties;
-                                    const found = markers.find(country => country["name"] === NAME);
-                                    console.log(found)
-                                    console.log()
-                                    if (found) {
-                                        const GREETING = found["greeting"];
-                                        setTooltipContent(`"${GREETING}" - ${NAME}`)
-                                    } else {
-                                        setTooltipContent(`${NAME}`)
-                                    }
-                                }}
-                                onMouseLeave={() => {
-                                    setTooltipContent("");
-                                }}
-                                style={{
-                                    default: {
-                                        fill: "#D6D6DA",
-                                        outline: "none"
-                                      },
-                                      hover: {
-                                        fill: "#F53",
-                                        outline: "none"
-                                      },
-                                      pressed: {
-                                        fill: "#E42",
-                                        outline: "none"
-                                      } 
-                                }}
-                                />
-                    )}
-                </Geographies>
-                {markers.map(({name, coordinates}) => (
-                    <Marker key={name} coordinates={coordinates}>
-                        <circle r={8} fill="#F53" />
-                    </Marker>
-                ))}
-            </ComposableMap>
+        
+        <div className="map-container">
+            <div className="map-filters">
+                <button className="map-filter-btn" type="">'Merry Christmas' in different languages</button>
+                <button className="map-filter-btn" type="">What day is Christmas celebrated?</button>
+                <button className="map-filter-btn" type="">What's the traditional Christmas meal?</button>
+            </div>
+            <div className="card">
+                <ComposableMap data-tip="" width="700" height="500" projectionConfig={{ scale: 190 }}>
+                    <Geographies geography={geoUrl}>
+                        {({geographies}) => geographies.map(geo =>
+                                <Geography 
+                                    key={geo.rsmKey} 
+                                    geography={geo} 
+                                    onMouseEnter={() => {
+                                        const { NAME } = geo.properties;
+                                        
+                                        const found = markers.find(country => country["name"] === NAME);
+                                        console.log(found)
+                                        console.log()
+                                        if (found) {
+                                            const GREETING = found["greeting"];
+                                            setTooltipContent(`"${GREETING}" - ${NAME}`)
+                                        } else {
+                                            setTooltipContent(`${NAME}`)
+                                        }
+                                    }}
+                                    onMouseLeave={() => {
+                                        setTooltipContent("");
+                                    }}
+                                    style={{
+                                        default: {
+                                            fill: "#D6D6DA",
+                                            outline: "none"
+                                        },
+                                        hover: {
+                                            fill: "#F53",
+                                            outline: "none"
+                                        },
+                                        pressed: {
+                                            fill: "#E42",
+                                            outline: "none"
+                                        } 
+                                    }}
+                                    />
+                        )}
+                    </Geographies>
+                    {markers.map(({name, coordinates}) => (
+                        <Marker key={name} coordinates={coordinates}>
+                            <a href=""><circle r={8} fill="#F53" /></a>
+                            <text textAnchor="middle" y="-11" style={{ fontSize: "8px" }}>{name}</text>
+                        </Marker>
+                    ))}
+                </ComposableMap>    
+            </div>
         </div>
     </>
     )
