1019b77c5178ba84ee7503d095b4f3b0914c1843
Heather King <hkingdevelopment@gmail.com>
2021-12-04 15:30:25 +0000

    copies over basic map set-up from test file
    includes install of react-simple-maps and react-tooltip

--- client/src/components/Map.js
+++ client/src/components/Map.js
@@ -1,9 +1,75 @@
-import React from "react";
+import React, {memo} from "react";
+import { ReactDOM } from "react";
+import { 
+    ComposableMap, 
+    Geographies, 
+    Geography, 
+    Marker
+} from "react-simple-maps";
+import "../css/map.css"
 
-const Map = () => {
+
+const geoUrl = "https://raw.githubusercontent.com/zcreativelabs/react-simple-maps/master/topojson-maps/world-110m.json";
+
+const markers = [
+    {name: "United States of America", coordinates: [-74.006, 40.7128], greeting: "Happy Holidays"},
+    {name: "United Kingdom", coordinates: [-3.19648, 55.95206], greeting: "Merry Christmas"}
+];
+
+
+
+const Map = ( {setTooltipContent} ) => {
 
     return (
-        <h2>I am the map!</h2>
+        <>
+        <h1>Map</h1>
+        <div>
+            <ComposableMap data-tip="" projectionConfig={{ scale: 200 }}>
+                <Geographies geography={geoUrl}>
+                    {({geographies}) => geographies.map(geo =>
+                            <Geography 
+                                key={geo.rsmKey} 
+                                geography={geo} 
+                                onMouseEnter={() => {
+                                    const { NAME } = geo.properties;
+                                    const found = markers.find(country => country["name"] === NAME);
+                                    console.log(found)
+                                    console.log()
+                                    if (found) {
+                                        const GREETING = found["greeting"];
+                                        setTooltipContent(`"${GREETING}" - ${NAME}`)
+                                    } else {
+                                        setTooltipContent(`${NAME}`)
+                                    }
+                                }}
+                                onMouseLeave={() => {
+                                    setTooltipContent("");
+                                }}
+                                style={{
+                                    default: {
+                                        fill: "#D6D6DA",
+                                        outline: "none"
+                                      },
+                                      hover: {
+                                        fill: "#F53",
+                                        outline: "none"
+                                      },
+                                      pressed: {
+                                        fill: "#E42",
+                                        outline: "none"
+                                      } 
+                                }}
+                                />
+                    )}
+                </Geographies>
+                {markers.map(({name, coordinates}) => (
+                    <Marker key={name} coordinates={coordinates}>
+                        <circle r={8} fill="#F53" />
+                    </Marker>
+                ))}
+            </ComposableMap>
+        </div>
+    </>
     )
 }
 
--- client/src/containers/BBCSleighRide.js
+++ client/src/containers/BBCSleighRide.js
@@ -1,4 +1,6 @@
 import React, {useState, useEffect} from "react";
+import ReactTooltip from 'react-tooltip';
+
 import Map from "../components/Map";
 import Header from "../components/Header";
 import Quiz from "../components/Quiz";
@@ -6,12 +8,18 @@ import Joke from "../components/Joke";
 import Footer from "../components/Footer";
 
 
+
+
 const BBCSleighRide = () => {
+
+    const [content, setContent] = useState("");
+
 
     return (
         <>
         <Header/>
-        <Map/>
+        <Map setTooltipContent={setContent}/>
+        <ReactTooltip>{content}</ReactTooltip>
         <Quiz/>
         <Joke />
         <Footer />
