9ef36ad51424953002e3289fc3d0da31f2cce9ff
Jean-Pierre Gazian <jeanpierregazian@msn.com>
2021-12-05 21:38:58 +0000

    committing with some of the joke component setup - needs to be completed tomorrow

--- client/src/containers/JokeContainer.js
+++ client/src/containers/JokeContainer.js
@@ -3,12 +3,26 @@ import Joke from "../components/Joke";
 import Header from "../components/Header";
 import Footer from "../components/Footer";
 
-const JokeContainer = () => {
+const JokeContainer = ({joke}) => {
+    const [jokes,setJokes] = useState([]);
+    const [randomJoke, setRandomJoke] = useState(null);
+
+    const getJokes = function(){
+        fetch('https://api.sleighride.com/jokes')
+        .then(res => res.json())
+        .then(jokes => setJokes(jokes))
+    }
+
+    onButtonClick = function (joke){
+        setRandomJoke(joke)
+    }
+
+
 
     return (
         <>
         <Header/>
-        <Joke />
+        <Joke onButtonClick = {onButtonClick} jokes = {jokes}/>
         <Footer />
         </>
     )
