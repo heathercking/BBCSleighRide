93668704b4a59f5a5eb10f7ea91ea0c9eb53aa44
Heather King <hkingdevelopment@gmail.com>
2021-12-05 16:56:36 +0000

    adds MapContainer and moves container functions from SleighRide to MapContainer

--- client/src/containers/BBCSleighRide.js
+++ client/src/containers/BBCSleighRide.js
@@ -1,44 +1,25 @@
 import React, {useState, useEffect} from "react";
-import ReactTooltip from 'react-tooltip';
 
 import Map from "../components/Map";
 import Header from "../components/Header";
 import Quiz from "../components/Quiz";
 import Joke from "../components/Joke";
 import Footer from "../components/Footer";
-
+import MapContainer from "./MapContainer";
 
 
 
 const BBCSleighRide = () => {
 
-    const [countries, setCountries] = useState([]);
-    const [content, setContent] = useState("");
-    const [selectedMapFilter, setSelectedMapFilter] = useState("greeting");
 
-    useEffect(() => {
-        getCountries();
-    }, [])
-
-
-    const handleSelectedMapFilter = (filter) => {
-        setSelectedMapFilter(filter);
-    }
-
-    const getCountries = function() {
-        fetch('http://localhost:5000/api/countries')
-        .then(res => res.json())
-        .then(countries => setCountries(countries))
-    }
 
 
     return (
         <>
         <Header/>
-        <Map countriesData={countries} setTooltipContent={setContent} onFilterSelect={handleSelectedMapFilter} chosenFilter={selectedMapFilter} />
-        <ReactTooltip type="dark" effect="float" multiline={true} html={true} border={true} borderColor="#D20018" scrollHide={true}>{content}</ReactTooltip>
+        <MapContainer/>
         <Quiz/>
-        <Joke />
+        <Joke/>
         <Footer />
         </>
     )
