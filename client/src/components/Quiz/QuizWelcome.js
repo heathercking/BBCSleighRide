3b1e04bed364605c69dedbe3fd0f24dc78db7c84
O-Marsters-1997 <olly.marsters@yahoo.co.uk>
2021-12-06 22:29:06 +0000

    applied basic styling to elf

--- client/src/components/Quiz/QuizWelcome.js
+++ client/src/components/Quiz/QuizWelcome.js
@@ -1,4 +1,7 @@
 import react from 'react';
+import quizCracker from '../../assets/images/christmas_cracker_quiz.svg';
+import quizElf from '../../assets/images/elf_placeholder.svg';
+
 // import '../../css/Quiz.css';
 
 const QuizWelcome = ({onButtonClick}) => {
@@ -11,8 +14,14 @@ const QuizWelcome = ({onButtonClick}) => {
     return (
         <div className="quiz-welcome">
             <h2>Welcome to the Christmas quiz</h2>
-            <p>Answer all the questions to be on the nice list</p>
-            <button onClick={handleClick}>Enter quiz</button>
+            <div className="quiz-welcome-content-wrapper">
+                <div className="quiz-welcome-text-wrapper">
+                    <p>Buddy the elf loves candy canes, get answers right to help him keep his candy</p>
+                    <p className="quiz-welcome-start-instructions">Click the button below to start</p>
+                    <img className="quiz-start-svg" src={quizCracker} alt="Christmas cracker image for quiz" onClick={handleClick} />
+                </div>
+                <img className="quiz-welcome-elf" src={quizElf} alt="Christmas elf for page load"/>
+            </div>
         </div>
         
     )
