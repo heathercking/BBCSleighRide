320691a1647552ed4c329ca37c9a7bf07d15dfaf
O-Marsters-1997 <olly.marsters@yahoo.co.uk>
2021-12-06 23:54:53 +0000

    applied basic styling to quiz question card

--- client/src/components/Quiz/QuizQuestion.js
+++ client/src/components/Quiz/QuizQuestion.js
@@ -5,6 +5,8 @@ import {useNavigate} from 'react-router-dom';
 import {nextQuestion} from '../../services/QuizLogic'
 import QuizTally from './QuizTally'
 import candy_cane from '../../assets/images/candy_cane.svg';
+import cracker_next from '../../assets/images/christmas_cracker_next.svg';
+import cracker_exit from '../../assets/images/christmas_cracker_exit.svg';
 
 
 const QuizQuestion = ({questions, question, score, answeredQuestions, removeQuizQuestion, replayQuiz, updateScore, shuffleArray, onAnswerCheck}) => {
@@ -25,17 +27,20 @@ const QuizQuestion = ({questions, question, score, answeredQuestions, removeQuiz
     //     shuffleArray(options)
     // }
 
+    const removeSelectedAnswer = () => {
+        const elsF =document.getElementsByClassName("selected-false");
+            while (elsF[0]) {
+                elsF[0].classList.remove("selected-false")
+            }
+        const elsT =document.getElementsByClassName("selected-true");
+            while (elsT[0]) {
+                elsT[0].classList.remove("selected-true")
+            }
+    }
+
     const handleClick = (event) => {
         const selectedElement = event.target;
-        const elsF =document.getElementsByClassName("selected-false");
-        while (elsF[0]) {
-            elsF[0].classList.remove("selected-false")
-        }
-        const elsT =document.getElementsByClassName("selected-true");
-        while (elsT[0]) {
-            elsT[0].classList.remove("selected-true")
-        }
-        
+        removeSelectedAnswer()
         if (event.target.innerHTML == question.correct) {
             setQuizAnswerIsCorrect(true);
             selectedElement.classList.add("selected-true");
@@ -49,7 +54,7 @@ const QuizQuestion = ({questions, question, score, answeredQuestions, removeQuiz
 
     const handleNext = (event) => {
         const newTotal = 0
-
+        removeSelectedAnswer()
         if (quizAnswerIsCorrect) {
             updateScore(score.correctQuestions + 1, score.incorrectQuestions, score.totalQuestions + 1)
         } else {
@@ -71,22 +76,27 @@ const QuizQuestion = ({questions, question, score, answeredQuestions, removeQuiz
 
     if (remainingGuesses) {
         return (
-            <div className="quiz-question">
-                <h4>{question.question}</h4>
+            <>
+            <div className="candy-canes">
                 {candyCanes}
-                <p>{question.correct}</p>
+            </div>
+            <div className="quiz-question">
+                {/* <BsFillCheckCircleFill  style={{display: quizAnswerIsCorrect ? 'block' : 'none'}}></BsFillCheckCircleFill>
+                <BsFillXCircleFill  style={{display: quizAnswerIsCorrect == false ? 'block' : 'none'}}></BsFillXCircleFill> */}
+                <h4>{question.question}</h4>
+                
+                {/* <p>{question.correct}</p> */}
                 <ul>
                     <li className="quiz-question-option quiz-option1" onClick = {handleClick}>{question.options[0]}</li>
                     <li className="quiz-question-option quiz-option1" onClick = {handleClick}>{question.options[1]}</li>
                     <li className="quiz-question-option quiz-option3" onClick = {handleClick}>{question.options[2]}</li>
                 </ul>
-                <button onClick = {handleNext}>Next</button>
-                <button onClick = {handleExit}>Exit</button>
-
-            <BsFillCheckCircleFill  style={{display: quizAnswerIsCorrect ? 'block' : 'none'}}></BsFillCheckCircleFill>
-            <BsFillXCircleFill  style={{display: quizAnswerIsCorrect == false ? 'block' : 'none'}}></BsFillXCircleFill>
-
             </div>
+            <div className="quiz-cracker-wrapper">
+                    <img className="quiz-question-nav-crackers" src={cracker_next} alt="Cracker button to symbolise next" onClick = {handleExit}></img>
+                    <img className="quiz-question-nav-crackers" src={cracker_exit} alt="Cracker button to symbolise exit" onClick = {handleExit}></img>
+                </div>
+            </>
         )
 
     } else {
