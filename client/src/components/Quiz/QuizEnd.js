54c9c893987156945fb87eb179fc07c37782e858
O-Marsters-1997 <olly.marsters@yahoo.co.uk>
2021-12-06 19:18:23 +0000

    added route to home page from the end of the quiz

--- client/src/components/Quiz/QuizEnd.js
+++ client/src/components/Quiz/QuizEnd.js
@@ -1,14 +1,20 @@
 import react from 'react';
-
+import {useNavigate} from 'react-router-dom';
 import QuizScore from './QuizScore';
 
 const QuizEnd = ({questionsRemaining, score})  => {
+
+    let navigate = useNavigate();
+
+    const handleExit = () => {
+        navigate('/')
+    }
     return (
          <div className="end-of-quiz">
             <h2>You have finished the quiz hooray!</h2>
             <QuizScore score = {score}/>
-            <button>Play Again</button>
-            <button>Exit</button>
+            <button >Play Again</button>
+            <button onClick={handleExit}>Exit</button>
         </div>
     )
 }
