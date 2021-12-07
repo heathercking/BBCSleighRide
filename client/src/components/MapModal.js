91204a373f53aac59595bb7012007a44979ad31e
Heather King <hkingdevelopment@gmail.com>
2021-12-07 20:18:25 +0000

    adds stylingt to mapModal according to jokeModal

--- client/src/components/MapModal.js
+++ client/src/components/MapModal.js
@@ -1,28 +1,28 @@
 import React from "react";
 import "../css/mapmodal.css";
+import cracker_exit from '../assets/images/christmas_cracker_exit.svg';
 
 const MapModal = ({setOpenModal}) => {
 
     return (
         <>
-        <div className="modalBackground">
-            <div className="modalContainer">
+        <div className="map-modal">
+            <div className="map-modal-content">
+                <div className="map-modal-header">
 
-                <div className="title">
-                    <h1>Take Santa for a Sleigh Ride!</h1>
+                    <h1 className="map-modal-title">Take Santa for a Sleigh Ride!</h1>
+
+                    <div className="map-modal-body">
+                        <p>1) Select a bauble </p>
+                        <p>2) Land Santa's Sleigh by each candy cane</p>
+                        <p>3) See what you can find out about Christmas in that country! </p>
+                    </div>
+
+                    <div className="map-modal-footer">
+                        <img src={cracker_exit} alt="Exit" className="map-exit-button" onClick={() => {setOpenModal(false);}}/>
+                    </div>
+
                 </div>
-
-                <div className="body">
-                    <p>Select a bauble </p>
-                    <p>Move the sligh to each map pin </p>
-                    <p>See what you can find out about Christmas in that country! </p>
-                </div>
-
-                <div className="footer">
-                    <button onClick={() => {setOpenModal(false);}} id="cancelBtn">Cancel</button>
-                    <button>Continue</button>
-                </div>
-
             </div>
         </div>
       </>
