56aceeb93471b6432548ceafea59f9ce225f071c
Heather King <hkingdevelopment@gmail.com>
2021-12-08 14:03:40 +0000

    updates joke modal styling to match map modal

--- client/src/containers/JokeContainer.js
+++ client/src/containers/JokeContainer.js
@@ -3,6 +3,7 @@ import Joke from "../components/Joke";
 import { getJokes } from '../services/SleighRideService';
 import '../css/JokeContainer.css'
 import cracker_exit from '../assets/images/christmas_cracker_exit.svg';
+import santa from '../assets/images/santa_happy.svg';
 
 const JokeContainer = ({setOpenModal}) => {
 
@@ -39,36 +40,34 @@ const JokeContainer = ({setOpenModal}) => {
       setJokeTally(jokeTally + 1)
     }
 
-const nodeItems = shuffledJokes.map(joke => {
+  const nodeItems = shuffledJokes.map(joke => {
+          return (
+              <Joke joke={joke} />
+          )
+  })
 
-        return (
-            <Joke joke={joke} />
-        )
-    })
 
-
-    return (
-        <>
-        <div className="modal">
-          <div className="modal-content">
-            <div className="modal-header">
-              <h2 className="modal-title">Welcome to the joke generator</h2>
-              <div className="modal-body">
-                {nodeItems}
-                  <p>Click the button below for a new joke</p>
-                  <button onClick={handleClick}>HoHoHo</button>
-              </div>
-              <div className="modal-footer">
-                <img src={cracker_exit} alt="Exit" className="exit-button" onClick={() => {
-              setOpenModal(false);
-            }}/>
-              </div>
+  return (
+      <>
+      <div className="modal">
+        <div className="modal-content">
+          <div className="modal-header">
+            <h1 className="modal-title">Joke Generator</h1>
+            <div className="modal-body">
+              {nodeItems}
+                <img src={santa} alt="next joke" className="joke-santa" onClick={handleClick}/>
+            </div>
+            <div className="modal-footer">
+              <img src={cracker_exit} alt="Exit" className="exit-button" onClick={() => {
+            setOpenModal(false);
+          }}/>
             </div>
           </div>
         </div>
+      </div>
 
-        </>
-    )
+      </>
+  )
 }
 
 export default JokeContainer;
